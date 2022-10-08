d6f56b2bec38dd6df98ebe9f3b93bedce4e8e433
wsvaio <360117242@qq.com>
2022-10-08 14:33:20 +0800

    x

--- src/components/vpopup/index.d.ts
+++ src/components/vpopup/index.d.ts
@@ -1,8 +1,10 @@
 import { FormProps, PopupProps } from "vant";
 
-type vpopupCtx = {
-  form: obj,
-  formProps: Partial<FormProps> & obj,
-  popup: Partial<PopupProps> & obj & { slot?: string, name?: string },
-  action: (name?: string) => Promise<T>
+declare global {
+  type vpopupCtx = {
+    form: obj,
+    formProps: Partial<FormProps> & obj,
+    popup: Partial<PopupProps> & obj & { slot?: string, name?: string },
+    action: (name?: string) => Promise<T>
+  }
 }
--- src/components/vtable-column/index.vue
+++ src/components/vtable-column/index.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { StyleValue } from "vue";
 
 const props = defineProps<{
   label?: string;
