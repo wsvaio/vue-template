75a5a33106f548bd74eeb187c8cb41ddd193905e
wsvaio <360117242@qq.com>
2022-10-18 15:12:47 +0800

    perf: 添加stylelint配置，添加husky配置，添加commitlint配置，添加lintstagedrc配置

--- .lintstagedrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "*.{js,ts,vue}": "eslint",
-  // ".{js,ts,vue}": "eslint --fix",
-  // ".{js,ts,vue}": "eslint --fix --max-warnings 0",
-
-  "*.{vue,css,less}": "stylelint",
-}
--- src/routes/admin/components/header/index.vue
+++ src/routes/admin/components/header/index.vue
@@ -19,7 +19,6 @@ const handleCommand = (command: T) => {
 };
 
 const submit = async ({}: vdialogCtx) => {
-
   return true;
 };
 
